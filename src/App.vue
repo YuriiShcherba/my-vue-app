a6b3c0c6245d90e9605612d9bba9a61962beb268
Yurii Scherba <gostlogic@gmail.com>
2025-08-07 11:33:48 +0300

    welcome template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
+    import Welcome from './components/pages/Welcome.vue';
 </script>
 
 <template>
-
+    <Welcome></Welcome>
 </template>
 
 <style scoped>
--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -3,6 +3,27 @@
 </script>
 
 <template>
+    <section id="welcome">
+        <div class="benefits">
+            <h2>Complete this program <br/>if you want...</h2>
+            <div>
+                <p>✅ Follow the program with proven results.</p>
+                <p>✅ Get fit, healthy, strong and absolutely shredded.</p>
+                <p>✅ Learn more about gym, training and technique</p>
+            </div>
+            <div>
+                <h3>The training plan</h3>
+                <p>This training plan follows a structure known as the <strong>Bro Split</strong></p>
+                <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
+            </div>
+            <div class="card">
+                <h3>🔥 Your Challenge</h3>
+                <p>Complete all the workouts and track your progress along the way!</p>
+                <p>Do you accept?</p>
+                <button>Begin &rarr;</button>
+            </div>
+        </div>
+    </section>
 
 </template>
 
